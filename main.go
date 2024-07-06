5dbf70d45c4d242052fc55b31606fdd10965c67e
Richard Cornwell <rich@sky-visions.com>
2024-07-06 13:40:34 -0400

    Main, now attaches reader to deck name in first argument. Entering a line will IPL card reader. Telnet server started on port 3270.

--- main.go
+++ main.go
@@ -1,12 +1,110 @@
+/*
+ * S370 - Main process.
+ *
+ * Copyright 2024, Richard Cornwell
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
-	CPU "github.com/rcornwell/S370/emu/cpu"
+	"github.com/rcornwell/S370/config/register"
+	"github.com/rcornwell/S370/emu/core"
+	device "github.com/rcornwell/S370/emu/device"
+	master "github.com/rcornwell/S370/emu/master"
+	"github.com/rcornwell/S370/emu/memory"
+	syschannel "github.com/rcornwell/S370/emu/sys_channel"
+	telnet "github.com/rcornwell/S370/telnet"
+
+	_ "github.com/rcornwell/S370/emu/models"
 )
 
 func main() {
-	fmt.Println("Hello World!")
-	CPU.InitializeCPU()
+	file := os.Args[1]
+
+	if file == "" {
+		fmt.Println("Please specify a file to load from")
+		return
+	}
+
+	// Create listener session
+	telServer, err := telnet.NewServer(":3270")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	masterChannel := make(chan master.Packet)
+
+	memory.SetSize(64)
+	cpu := core.NewCPU(masterChannel)
+	// Configure I/O devices.
+	syschannel.InitializeChannels()
+	syschannel.AddChannel(0, device.TypeMux, 192)
+	register.CreateModel("1052", 0x01f)
+	register.CreateModel("2540R", 0x00c)
+
+	// Start telnet server.
+	telServer.Start(masterChannel)
+
+	// Start main emulator.
+	go cpu.Start()
+
+	// Wait for a SIGINT or SIGTERM signal to gracefully shut down the server
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	msg := make(chan string, 1)
+	go func() {
+		reader := bufio.NewReader(os.Stdin)
+		// Receive commands from stdin
+		for {
+			input, _ := reader.ReadString('\n')
+			msg <- input
+		}
+	}()
+
+loop:
+	for {
+		select {
+		case <-sigChan:
+			fmt.Println("Got quit signal")
+			break loop
+		case input := <-msg:
+			syschannel.Detach(0x00c)
+			syschannel.Attach(0x00c, file)
+			fmt.Printf("IPL device: %s\n", input)
+			masterChannel <- master.Packet{DevNum: 0x00c, Msg: master.IPLdevice}
+		}
+	}
+
+	fmt.Println("Shutting down CPU")
+	cpu.Stop()
+	fmt.Println("Shutting down server...")
+	telServer.Stop()
+	fmt.Println("Servers stopped.")
 }
